e88bbac99e74c1ca727da0fc29de75553db205c1
hdfs <hdfs@Mac-mini-de-KMMX-07.local>
2018-01-13 14:12:12 -0600

    tarea

--- C4-3.py
+++ C4-3.py
@@ -5,3 +5,8 @@ cadena = raw_input("Ingresa tu nombre: ")
 
 if (x.isaplha() for x in cadena):
 	print (x)
+
+
+	rfc consonante - vocal 
+	que desayunaste dar opcion y siselecciona dile que es saludable, sino mandalo al nutriologo
+	hola mundo separar vocales y consonantes
